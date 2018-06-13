7f6dc0df8f556a88e1f44d250d867a9bcb7efd85
Robinson Huacho <robinson.huacho@epn.edu.ec>
2018-06-12 23:43:34 -0500

    Inclusión Logo

--- Tesis_PrototipoMediaFidelidad_V1.0/app/src/main/java/com/epn/robinsonhuacho/tesis_prototipomediafidelidad_v10/IngresoAplicacion.java
+++ Tesis_PrototipoMediaFidelidad_V1.0/app/src/main/java/com/epn/robinsonhuacho/tesis_prototipomediafidelidad_v10/IngresoAplicacion.java
@@ -9,5 +9,6 @@ public class IngresoAplicacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_aplicacion);
+
     }
 }
